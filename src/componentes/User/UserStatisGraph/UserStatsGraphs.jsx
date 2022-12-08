a2e6c07c87bf6aebfeb1a6ad4b808578318445a7
Alquimara Alves <alquimara.a.braz@gmail.com>
2022-12-08 15:36:28 -0300

    adicionado os graficos no componente de UserStatis

--- src/componentes/User/UserStatisGraph/UserStatsGraphs.jsx
+++ src/componentes/User/UserStatisGraph/UserStatsGraphs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styles from './UserStatisGraph.module.css'
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory'
 
 const UserStatsGraphs = ({ data }) => {
   const [statsGraph, setStatsGraph] = useState([])
@@ -10,11 +11,47 @@ const UserStatsGraphs = ({ data }) => {
         .map(({ acessos }) => Number(acessos))
         .reduce((acc, acessos) => acc + acessos)
     )
+    setStatsGraph(
+      data.map(item => {
+        return {
+          x: item.title,
+          y: item.acessos
+        }
+      })
+    )
   }, [data])
   return (
     <section className={`animeLeft ${styles.graph}`}>
-      <div className={styles.total}>
+      <div className={`${styles.total} ${styles.graphItem}`}>
         <p>Acessos:{total}</p>
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryPie
+          data={statsGraph}
+          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+          style={{
+            data: {
+              fillOpacity: 0.9,
+              stroke: '#fff',
+              strokeWidth: 2,
+              fill: '#344'
+            },
+
+            labels: {
+              fontSize: 14,
+              fill: '#333'
+            }
+          }}
+        />
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryChart>
+          <VictoryBar
+            alignment="start"
+            data={statsGraph}
+            colorScale={['gold', 'orange', 'tomato']}
+          />
+        </VictoryChart>
       </div>
     </section>
   )
